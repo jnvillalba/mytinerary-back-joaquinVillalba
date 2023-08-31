c5e28a74904a79207e697ed64fb9edad32eec858
Joaquin Villalba <joaquinvillalba8@gmail.com>
2023-08-31 18:27:47 -0300

    router controller Itinerary

--- src/middlewares/verfications.js
+++ src/middlewares/verfications.js
@@ -15,6 +15,41 @@ const validateCityData = (req, res, next) => {
   next();
 };
 
+const validateItineraryData = (req, res, next) => {
+  const data = req.body;
+
+  if (!data) {
+    return res.status(400).json({ message: "Invalid data" });
+  }
+  if (!data.name) {
+    return res.status(400).json({ message: "The 'name' field is required" });
+  }
+
+  if (!data.user) {
+    return res.status(400).json({ message: "The 'user' field is required" });
+  }
+
+  if (!data.city) {
+    return res.status(400).json({ message: "The 'city' field is required" });
+  }
+
+  if (typeof data.price !== "number" || data.price < 1 || data.price > 5) {
+    return res.status(400).json({ message: "The 'price' field must be a number between 1 and 5" });
+  }
+
+  if (typeof data.durationHours !== "number" || data.durationHours <= 0) {
+    return res.status(400).json({ message: "The 'durationHours' field must be a number greater than 0" });
+  }
+
+  if (!data.hashtags || !Array.isArray(data.hashtags) || data.hashtags.length === 0) {
+    return res.status(400).json({ message: "The 'hashtags' field must be a non-empty array of strings" });
+  }
+
+  next();
+};
+
+
 module.exports = {
   validateCityData,
+  validateItineraryData
 };
--- src/router/router.js
+++ src/router/router.js
@@ -7,15 +7,33 @@ const {
   deleteCity,
   updateCity,
 } = require("../controllers/cityController");
-const { validateCityData } = require("../middlewares/verfications");
+const {
+  getItineraries,
+  getItineraryById,
+  addItinerary,
+  deleteItinerary,
+  updateItinerary,
+  getItinerariesByCity,
+} = require("../controllers/itineraryController");
+const {
+  validateCityData,
+  validateItineraryData,
+} = require("../middlewares/verfications");
+//City
 router.get("/city/:id", getCityById);
 router.get("/cities", getCities);
-
 router.post("/city", validateCityData, addCity);
-
 router.put("/city/:id", updateCity);
-
-
 router.delete("/city/:id", deleteCity);
+
+//Itinerary
+
+router.get("/itinerary/:id", getItineraryById);
+router.get("/itineraries/:cityId", getItinerariesByCity);
+
+router.get("/itinerary", getItineraries);
+router.post("/itinerary", validateItineraryData, addItinerary);
+router.put("/itinerary/:id", updateItinerary);
+router.delete("/itinerary/:id", deleteItinerary);
 
 module.exports = router;
